47904133e6e5290d763202ef1e5b79a17bbb65bc
Adam Di Carlo <adam@onshored.com>
2017-08-01 11:28:02 -0400

    some baseline widget assumptions validated

--- tests/test_bargraph_widget.py
+++ tests/test_bargraph_widget.py
@@ -3,8 +3,19 @@ import pylcdproc
 import lcdtesthelper
 
 class TestBargraphWidget(lcdtesthelper.WidgetLCDTest):
-    def test_base(self):
-        w = self.lcd.HBargraph(x=1, y=1, length=self.lcd.width)
+    def test_hbase(self):
+        old = len(self.lcd.widgets)
+        w = self.lcd.HBargraph(x=1, y=1, length=(self.lcd.width - 1))
+        self.assertTrue(w.lcd)
+        self.assertEqual(w.wid, "hbar1")
+        self.assertEqual(len(self.lcd.widgets), old + 1)
+
+    def test_vbase(self):
+        old = len(self.lcd.widgets)
+        w = self.lcd.VBargraph(x=self.lcd.width, y=self.lcd.height, length=self.lcd.height)
+        self.assertTrue(w.lcd)
+        self.assertEqual(w.wid, "vbar1")
+        self.assertEqual(len(self.lcd.widgets), old + 1)
 
 if __name__ == '__main__':
     unittest.main()
