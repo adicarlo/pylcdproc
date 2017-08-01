a391042aedd5e576363d46452caf057fcc388634
Adam Di Carlo <adam@onshored.com>
2017-08-01 11:19:42 -0400

    update and modularize

--- demo.py
+++ demo.py
@@ -3,28 +3,18 @@
 import time
 import pylcdproc
 
-def main():
-    # lcd = pylcdproc.BaseLCD(appname="test_pylcdproc", host="gw.coo", debug=True)
-    # print(lcd.summarize())
-    # wid = 'msg1'
-    # lcd.widget_add(wid, 'string')
-    # lcd.widget_set(wid, 1, 1, 'testing 1 2 3 ...')
-    # time.sleep(5)
-    # lcd.dispose()
-
+def demo_icons():
     lcd = pylcdproc.IconFieldLCD(appname="demo", host="gw.coo", debug=True)
     print(lcd.summarize())
     print("filling blocks...")
     lcd.icon_fill('BLOCK_FILLED')
     print("filling checkbox ...")
     lcd.icon_fill('CHECKBOX_ON')
-
-    print("delay...")
-    time.sleep(5)
-
-    # FIXME: this one fails (but only if prior icon_fill ran)
     print("running a block...")
-    lcd.icon_run('BLOCK_FILLED')
+    lcd.icon_run('BLOCK_FILLED', 'HEART_OPEN')
+
+def main():
+    demo_icons()
 
 if __name__ == "__main__":
     main()
