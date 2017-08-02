e2285ae27ec9fb4e52058e297344ac897d700a90
Adam Di Carlo <adam@onshored.com>
2017-08-01 22:41:37 -0400

    i guess our widget numbering is zero based?

--- tests/test_bargraph_widget.py
+++ tests/test_bargraph_widget.py
@@ -7,7 +7,7 @@ class TestBargraphWidget(lcdtesthelper.WidgetLCDTest):
         old = len(self.lcd.widgets)
         w = self.lcd.HBargraph(x=1, y=1, length=(self.lcd.width - 1))
         self.assertTrue(w.lcd)
-        self.assertEqual(w.wid, "hbar1")
+        self.assertEqual(w.wid, "hbar0")
         self.assertEqual(len(self.lcd.widgets), old + 1)
 
     def test_multiple(self):
@@ -19,7 +19,7 @@ class TestBargraphWidget(lcdtesthelper.WidgetLCDTest):
         old = len(self.lcd.widgets)
         w = self.lcd.VBargraph(x=self.lcd.width, y=self.lcd.height, length=self.lcd.height)
         self.assertTrue(w.lcd)
-        self.assertEqual(w.wid, "vbar1")
+        self.assertEqual(w.wid, "vbar0")
         self.assertEqual(len(self.lcd.widgets), old + 1)
 
 if __name__ == '__main__':
