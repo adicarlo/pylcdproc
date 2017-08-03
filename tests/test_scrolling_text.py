ff7237535f9485253bde5c7d63aae6b6aa745e15
Adam Di Carlo <adam@onshored.com>
2017-08-03 01:34:14 -0400

    linty

--- tests/test_scrolling_text.py
+++ tests/test_scrolling_text.py
@@ -1,11 +1,13 @@
 import unittest
-import time
 import pylcdproc
 import lcdtesthelper
 
+
 class TestScrollingText(lcdtesthelper.StaticLCDTest):
-    def instantiateLCD(appname="testScrollingText", host=lcdtesthelper._default_test_host()):
-        # FIXME: is there any way to tramp the debug setting from debug verbosity?
+    def instantiateLCD(appname="testScrollingText",
+                       host=lcdtesthelper._default_test_host()):
+        # FIXME: is there any way to tramp the debug setting from test
+        # verbosity?  or even generalize it for all test cases?
         return pylcdproc.ScrollingTextLCD(appname, host, debug=False)
 
     def test_base(self):
@@ -18,6 +20,7 @@ class TestScrollingText(lcdtesthelper.StaticLCDTest):
 
     def test_long(self):
         self.lcd.display("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam vulputate sollicitudin scelerisque. Fusce ex erat, convallis ac arcu ac, auctor volutpat enim. Sed dignissim felis ac neque tristique, id sagittis diam semper. Vestibulum lacinia ante fringilla euismod aliquam. Morbi dapibus tincidunt massa in sollicitudin. Praesent feugiat metus vitae faucibus luctus. Phasellus venenatis tellus vitae lacinia laoreet. Praesent vitae tortor viverra, sodales leo id, malesuada sem. Etiam varius, leo id pretium iaculis, ante tortor interdum dolor, at convallis urna mi sit amet est.")
+
 
 if __name__ == '__main__':
     unittest.main()
