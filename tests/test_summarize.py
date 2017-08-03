36e39296b699913f382cd2da218480660f365216
Adam Di Carlo <adam@onshored.com>
2017-08-03 01:19:51 -0400

    linty

--- tests/test_summarize.py
+++ tests/test_summarize.py
@@ -1,6 +1,7 @@
-import pylcdproc
 import lcdtesthelper
+
 
 class TestSummarize(lcdtesthelper.BaseLCDTest):
     def test_summarize(self):
-        self.assertEqual(self.lcd.summarize(), "server protocol 0.3, lcd 20x2 chars with 5x8 cells")
+        self.assertEqual(self.lcd.summarize(),
+                         "server protocol 0.3, lcd 20x2 chars with 5x8 cells")
