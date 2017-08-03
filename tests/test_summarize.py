8e6e83cbe6ba80b451505ba9997b6a1fa207a245
Adam Di Carlo <adam@onshored.com>
2017-08-03 01:22:26 -0400

    fix overly fragile test

--- tests/test_summarize.py
+++ tests/test_summarize.py
@@ -3,5 +3,5 @@ import lcdtesthelper
 
 class TestSummarize(lcdtesthelper.BaseLCDTest):
     def test_summarize(self):
-        self.assertEqual(self.lcd.summarize(),
-                         "server protocol 0.3, lcd 20x2 chars with 5x8 cells")
+        r = r"server protocol 0.3, lcd [0-9x]+ chars with [0-9x]+ cells"
+        self.assertRegex(self.lcd.summarize(), r)
