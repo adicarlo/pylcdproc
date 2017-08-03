af7eefe01043e965b6aeb64850d831111cc42685
Adam Di Carlo <adam@onshored.com>
2017-08-03 01:27:53 -0400

    linty

--- tests/test_setup.py
+++ tests/test_setup.py
@@ -1,6 +1,6 @@
 import unittest
-import pylcdproc
 import lcdtesthelper
+
 
 class TestSetup(lcdtesthelper.BaseLCDTest):
     def test_setup_size(self):
@@ -12,9 +12,9 @@ class TestSetup(lcdtesthelper.BaseLCDTest):
         self.assertIsInstance(self.lcd.cell_height, int)
 
     def test_functional_setup(self):
-        self.assertTrue(self.lcd.setup(b"connect LCDproc 0.5.7 protocol 0.3 lcd wid 20 hgt 2 cellwid 5 cellhgt 8\n"))
+        s = b"connect LCDproc 0.5.7 protocol 0.3 lcd wid 20 hgt 2 cellwid 5 cellhgt 8\n"
+        self.assertTrue(self.lcd.setup(s))
 
 
 if __name__ == '__main__':
     unittest.main()
-
